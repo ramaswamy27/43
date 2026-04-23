8f59f1e78ec928a8a5f471cbfedbcd3b55f26a09
ramaswamy27 <ramaswamy_27@yahoo.com>
2026-04-23 08:54:47 +0530

    Change greeting message in Helloworld

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello Rama! How are you?");
+        System.out.println("Hello Rama! How are you doing?");
     }
 }
